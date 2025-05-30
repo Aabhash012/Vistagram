dabc47dd891301df9b800bd96d8e6cf2ba3a5b58
Aabhash Sharma <aabhash.aakash@gmail.com>
2025-05-30 23:29:26 +0530

    Setting up my repo.

---
+++ src/main/java/com/vistagram/app/VistagramApplication.java
@@ -0,0 +1,13 @@
+package com.vistagram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistagramApplication.class, args);
+	}
+
+}
